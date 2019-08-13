de2680a598e8c1f120503f0ceb01779fe769c87c
Antoine Gaillard <antoine@agaillard.fr>
2019-08-13 15:16:29 +0200

    all good after refacto

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Read All
+  get 'tasks', to: 'tasks#index'
+
+  # Create
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+
+  # Read One
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
